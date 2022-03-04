c982e1e70a418d5135721f205c21e690d6c637b4
JoshOBrien <joshmobrien@gmail.com>
2022-03-04 10:20:57 -0800

    Formatting

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,6 @@
 ## Runs tests in "../inst/tinytest/" (currently just
-## "../inst/tinytest/test_exiftoolr.R"
+## "../inst/tinytest/test_exiftoolr.R")
 
-if ( requireNamespace("tinytest", quietly=TRUE) ){
+if (requireNamespace("tinytest", quietly=TRUE)) {
   tinytest::test_package("exiftoolr")
 }
