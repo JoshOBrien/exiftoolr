3a23cdf190b772cf86a0c712cb696a24213eaa79
JoshOBrien <joshmobrien@gmail.com>
2022-06-09 00:11:12 -0700

    Remove tests

--- tests/tinytest.R
+++
@@ -1,6 +0,0 @@
-## Runs tests in "../inst/tinytest/" (currently just
-## "../inst/tinytest/test_exiftoolr.R")
-
-if (requireNamespace("tinytest", quietly=TRUE)) {
-  tinytest::test_package("exiftoolr")
-}
